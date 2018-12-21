6174f2a9a64d01fb26fa7b6d76f077ba3e04edbf
AWesthoff <eragon588@web.de>
2018-12-21 13:18:59 +0100

    new kafka receiver

--- src/main/java/edu/hm/dako/chat/benchmarking_kafka/Test.java
+++ src/main/java/edu/hm/dako/chat/benchmarking_kafka/Test.java
@@ -13,34 +13,6 @@ import java.util.Properties;
 
 public class Test {
 
-    public void getMessages(String name) {
-        //get Kafka Topic
-        Properties properties = new Properties();
-        properties.put("bootstrap.servers", "localhost:9092");
-        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put("value.deserializer", "edu.hm.dako.chat.common.ChatMessageDeserializer");
-        properties.put("group.id", name);//"test-group");
-        KafkaConsumer kafkaConsumer = new KafkaConsumer(properties);
-        List topics = new ArrayList();
-        topics.add("responseTopic");
-        kafkaConsumer.subscribe(topics);
-
-        Thread t1 = new Thread(() -> {
-            ConsumerRecords<String, ChatMessage> messages = kafkaConsumer.poll(1000);
-            for (ConsumerRecord<String, ChatMessage> omessage : messages) {
-                System.out.println("Message received " + omessage.value().toString());
-                ChatMessage message = omessage.value();
-                String chatName = message.getUserName();
-                String chatMessage = message.getMessage();
-                long RTT = System.currentTimeMillis() - message.getTimestamp();
-
-                System.out.println("ROUNDTRIPTIME " + RTT);
-                System.out.println(message.getUserName() + ":" + message.getMessage());
-            }
-
-        });
-        t1.start();
-    }
 
 
     public static void main(String args[]) throws IOException {
@@ -54,9 +26,9 @@ public class Test {
 
             kafkaChatClient.login(name);
 
-            test.getMessages(name);
+            //test.getMessages(name);
 
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < 100; j++) {
                 String message = "+++" + j;
 
                 kafkaChatClient.tell(name, message);
